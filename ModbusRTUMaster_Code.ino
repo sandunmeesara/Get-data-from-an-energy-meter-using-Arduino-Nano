e0542a9003408bc34dbffef9f087d88cff7880e5
sandunmeesara <sandunmeesara@gmail.com>
2024-01-03 16:21:33 +0530

    getting data from CHNT device succeful

--- ModbusRTUMaster_Code.ino
+++ ModbusRTUMaster_Code.ino
@@ -8,10 +8,7 @@ const uint8_t dePin = 4;
 SoftwareSerial mySerial(rxPin, txPin);
 ModbusRTUMaster modbus(mySerial, dePin); // serial port, driver enable pin for rs-485 (optional)
 
-//bool coils[4];
-//bool discreteInputs[4];
 uint16_t holdingRegisters[10];
-//uint16_t inputRegisters[4];
 uint32_t total,x;
 
 void setup() {
@@ -22,13 +19,6 @@ void setup() {
 
 void loop() {
   
-  //modbus.writeMultipleCoils(1, 0, coils, 2);                       // slave id, starting data address, bool array of coil values, number of coils to write
-  //modbus.writeMultipleHoldingRegisters(1, 0, holdingRegisters, 2); // slave id, starting data address, unsigned 16 bit integer array of holding register values, number of holding registers to write
-  //modbus.readDiscreteInputs(1, 0, discreteInputs, 2);              // slave id, starting data address, bool array to place discrete input values, number of discrete inputs to read
-  //modbus.readInputRegisters(1, 0, inputRegisters, 2);              // slave id, starting data address, unsigned 16 bit integer array to place input register values, number of input registers to read
-
-
-
   modbus.readHoldingRegisters(1, 0x01, holdingRegisters,1);
   x = holdingRegisters[0];
   Serial.println("Passcode : " + String(x) + "");
@@ -41,20 +31,43 @@ void loop() {
   x = holdingRegisters[0];
   Serial.println("Meter type (meter type) : " + String(x) + "");
 
-  modbus.readHoldingRegisters(1, 0x2044, holdingRegisters,2);
+  modbus.readHoldingRegisters(1, 0x2008, holdingRegisters,2);
   total = ((uint32_t)holdingRegisters[0]<<16) | holdingRegisters[1];
-  //float floatValue;
-  //memcpy(&floatValue,&total,sizeof(float));
-
-  //x = holdingRegisters[]
-  //x = holdingRegisters[0]<<16 | holdingRegisters[1];
-  Serial.println("Line line voltage (uab) : " + String(total) + "v");
-  
-  for(int i=0;i<2;i++){
-    Serial.println("Code: " + String(holdingRegisters[i]) + "");
-  }
+  String hexString = String(total, HEX);
+  float floatResult = hexToFloat(hexString);
+  floatResult = floatResult * 0.1;
+  Serial.println("Phase phase voltage (ub) : " + String(floatResult) + "v");
   
   Serial.println("--------------------------");
   delay(1000);
 
 }
+
+
+String decimalToHex(String decimalString) {
+  long decimalValue = atol(decimalString.c_str()); // Convert decimal string to long
+  char hexBuffer[9]; // Buffer for storing hexadecimal representation (8 characters + null terminator)
+  snprintf(hexBuffer, sizeof(hexBuffer), "%08lX", decimalValue); // Convert long to hexadecimal string
+  return String(hexBuffer);
+}
+
+bool isValidHex(String hexString) {
+  for (char c : hexString) {
+    if (!isHexadecimalDigit(c)) {
+      return false;
+    }
+  }
+  return hexString.length() > 0;
+}
+
+bool isHexadecimalDigit(char c) {
+  return (c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f');
+}
+
+float hexToFloat(String hexString) {
+  unsigned long hexInt = strtoul(hexString.c_str(), NULL, 16); // Convert hexadecimal string to unsigned long
+  float result;
+  memcpy(&result, &hexInt, sizeof(result)); // Copy the raw bytes into a float variable
+  return result;
+}
+
